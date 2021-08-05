2b90829d59401c17660aacee37e895f58113ff64
Mitchel <mitchel.okomor@gmail.com>
2021-08-05 19:37:32 +0100

    router

--- test/index.test.js
+++ test/index.test.js
@@ -3,11 +3,11 @@ import { expect, server, BASE_URL } from './setup';
 describe('Index page test', () => {
   it('gets base url', (done) => {
     server
-      .get(`${BASE_URL}/store`)
+      .get(`${BASE_URL}/`)
       .expect(200)
       .end((err, res) => {
         expect(res.status).to.equal(200);
-        expect(res.body.message).to.equal('Store routes is working!');
+        expect(res.body.message).to.equal('Home route is working!');
         done();
       });
   });
